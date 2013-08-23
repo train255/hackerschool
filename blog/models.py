90d716ef1385d8523b82feb258cd06fdcf65310b
Trung Ngo <ndtrung4419@gmail.com>
2013-08-23 11:08:13 +0700

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	"""docstring for Post"""
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	created_date = models.DateTimeField(auto_now_add=True)
+
