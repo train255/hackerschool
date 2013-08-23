a028e7e78ff28e7d7a62c82aee4ab3ed44a23d66
Trung Ngo <ndtrung4419@gmail.com>
2013-08-23 11:34:55 +0700

    Add view for each post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from blog.models import Post
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts' : posts})
 
+
+def post(request, slug):
+    # get the Post object
+    post = get_object_or_404(Post, slug=slug)
+    # now return the rendered template
+    return render(request, 'blog/post.html', {'post': post})
